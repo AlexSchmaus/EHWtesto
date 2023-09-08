9d59bc99cdda2be1190cb00968804eeb7de9ff2c
Alex Schmaus <94810049+AlexSchmaus@users.noreply.github.com>
2023-09-07 21:32:55 -0400

    Update app.js

--- app.js
+++ app.js
@@ -13,18 +13,7 @@ const html = `
 <!DOCTYPE html>
 <html>
   <head>
-    <title>Hello from Render!</title>
-    <script src="https://cdn.jsdelivr.net/npm/canvas-confetti@1.5.1/dist/confetti.browser.min.js"></script>
-    <script>
-      setTimeout(() => {
-        confetti({
-          particleCount: 100,
-          spread: 70,
-          origin: { y: 0.6 },
-          disableForReducedMotion: true
-        });
-      }, 500);
-    </script>
+    <title>Hello World</title>
     <style>
       @import url("https://p.typekit.net/p.css?s=1&k=vnd5zic&ht=tk&f=39475.39476.39477.39478.39479.39480.39481.39482&a=18673890&app=typekit&e=css");
       @font-face {
@@ -54,7 +43,7 @@ const html = `
   </head>
   <body>
     <section>
-      Hello from Render!
+      Chinese food is tasty
     </section>
   </body>
 </html>
